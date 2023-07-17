b21ac4568e2f925d697b96dfdb7b3d2a6fb5f37d
JiteshShrivastav01 <jitesh.cs01@gmail.com>
2023-07-17 20:22:39 +0530

    Task 5

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,52 +5,33 @@ import { useContext ,useState} from "react";
 
 const Main=()=>{
     const ctx=useContext(AuthContext)
-    const [userData, setUserData]=useState([])
-    const [showData,setShowData]=useState(false)
+    const [verify , setVerify]=useState(false)
 
-    const LoadData=()=>{
-        const res=fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyB2B4txBmWbj7nOJi7zyym2kuveqd3iews',{
+
+    const verifyEmail=async ()=>{
+        try{
+          const res=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyB2B4txBmWbj7nOJi7zyym2kuveqd3iews',{
             method:'POST',
             body:JSON.stringify({
-                idToken : ctx.token,
-            }),
-            headers:{
-                'Content-Type': 'application/json' 
-            }
-        })
-        .then((res) => {
-            if (res.ok) {
-             return res.json()
-            } else {
-              return res.json().then((data) => {
-                const error = 'Authentication Error';
-                throw new Error(error);
-              });
-            }
+              requestType : 'VERIFY_EMAIL',
+              idToken : ctx.token
+            })
           })
-          .then((data) => {
-            console.log(data.users)
-            setUserData(data.users)
-            setShowData(true)
-            console.log('Load Successfully.')
-          })
-          .catch((error) => {
-            alert(error.message);
-          });
+          const data=await res.json()
+          console.log(data)
+          setVerify(true)
+        }
+        catch(err){
+          alert(err)
+        }
     }
 
     return(
         <>
           <Navbar/>
-          <button onClick={LoadData} style={{marginTop:'5rem', padding:'0.5rem 1rem',borderRadius:'1rem',marginLeft:'3rem'}}>Load data</button>
-
-          {showData &&
-            <div>
-                <li>{userData[0].displayName}</li>
-                <li>{userData[0].email}</li>
-                <li>{userData[0].photoUrl}</li>
-            </div>            
-          }
+        
+        {!verify && <button onClick={verifyEmail} style={{marginTop:'5rem', padding:'0.5rem 1rem',borderRadius:'1rem',marginLeft:'3rem'}}>Verify Email</button>}
+          
         </>
     )
 }
